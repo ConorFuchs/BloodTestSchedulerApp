86c69245ecd07230095d216156c97cbe1a8d77f6
Conor <conorfuchs@hotmail.com>
2025-03-17 15:39:12 +0000

    Fourth commit, modified the MyPriorityQ class, made some changes to constructor and added a comparator to sort, added rest of methods in class

--- src/bloodtestercaconorfuchs/MyPriorityQ.java
+++ src/bloodtestercaconorfuchs/MyPriorityQ.java
@@ -4,6 +4,7 @@
  */
 package bloodtestercaconorfuchs;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -11,27 +12,42 @@ import java.util.PriorityQueue;
  * @author serpl
  */
 public class MyPriorityQ implements PriorityQInterface<Patient>{
-    private PriorityQueue<Patient> pq;
+    private PriorityQueue<Patient> pq; //holds priority queue of patients
 
-    public MyPriorityQ(PriorityQueue<Patient> pq) {
-        this.pq = pq;
+    //constructor
+    public MyPriorityQ() {
+        //using a comparator to assign priority to the patients 
+        pq = new PriorityQueue<>(Comparator
+        .comparing((Patient p) -> {
+            switch (p.getPriority()){
+                case "urgent": return 1; //urgent patients are 1st priority
+                case "medium": return 2; //2nd priority
+                default: return 3; //low priority last
+            }
+        })
+            //if patients are same priority, use age to compare, older is more priority
+            .thenComparing(p -> -p.getAge())
+            //if age is the same can check if theyre from hospital ward or not, patients not from one are higher prio
+            .thenComparing(p -> !p.isFromHospitalWard()));
     }
   
-    //
+    //add new patient to PQ
     @Override
-    public void insert(Patient patient){//will finish implementing this later just setting up
-        
+    public void insert(Patient patient){
+        pq.add(patient);
     }
     
+    //remove patient from PQ with highest prio as theyve been seen to
     @Override
     public Patient remove(){
-        return null;//here till i fix issue and create method
+        return pq.poll();
         
     }
     
+    //check if priority quehe is empty or not
     @Override
     public boolean isEmpty(){
-        return false; //here till i fix issue and create method
+        return pq.isEmpty();
     }
     
     
